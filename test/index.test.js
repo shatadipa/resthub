be9fb19de1761c0b9cab58c3f7ec145727a601da
Shatadipa Chakraborty <tuli@Shatadipas-MacBook-Pro.local>
2018-12-01 23:51:55 -0800

    Fixes test for pipeline

--- test/index.test.js
+++ test/index.test.js
@@ -6,12 +6,13 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Contacts', function () {
-    it('should list ALL contacts on /contacts GET', function(done) {
-        chai.request(index)
-          .get('/api/contacts')
-          .end(function(err, res){
-            res.should.have.status(200);
-            done();
-          });
+  it('should list ALL contacts on /contacts GET', function (done) {
+    chai.request(index)
+      .get('/api/contacts')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        done();
       });
+      done();
+  });
 });
