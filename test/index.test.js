dde3794b1e9e687af814a8376189fda0cc7980e4
Shatadipa Chakraborty <tuli@Shatadipas-MacBook-Pro.local>
2018-12-01 23:35:27 -0800

    Adds a simple mocha test

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var index = require('../index');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Contacts', function () {
+    it('should list ALL contacts on /contacts GET', function(done) {
+        chai.request(index)
+          .get('/api/contacts')
+          .end(function(err, res){
+            res.should.have.status(200);
+            done();
+          });
+      });
+});
